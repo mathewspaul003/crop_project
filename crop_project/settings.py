283b32c5e2342f5e13612501bc7e3e520732437b
mathewspaul003 <mathewspaul003@depaul.eu.in>
2026-02-27 09:31:28 +0530

    Final Vercel stability fix: use absolute routes and restore standard static settings

--- crop_project/settings.py
+++ crop_project/settings.py
@@ -134,12 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-
-# Ensure media folder files (like initial crop photos) are collected as static for Vercel
-STATICFILES_DIRS = [
-    BASE_DIR / "media",
-]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
